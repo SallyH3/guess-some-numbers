b8f60ed87b9fa1a7f12209aceebc48ca068a5f7c
Sally Haefling <sallyhaefling@gmail.com>
2019-05-16 16:27:16 -0600

    Error message

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
+let randomNum;
 let minField = document.querySelector('.js-min-input')
 let maxField = document.querySelector('.js-max-input')
 let updateBtn = document.querySelector('.update-btn')
-let minRangeNum = document.querySelector('.min-range-txt')
-let maxRangeNum = document.querySelector('.max-range-txt')
 let guessInput = document.querySelector('.js-guess')
 let submitBtn = document.querySelector('.submit-btn')
 let clearBtn = document.querySelector('.clear-btn')
@@ -11,6 +10,7 @@ let rightDisplayNum = document.querySelector('.right-side-num')
 
 resetBtn.addEventListener('click', resetGame)
 guessInput.addEventListener('keyup', enableButtons)
+updateBtn.addEventListener('click', updateValues)
 
 function enableButtons () {
   if(guessInput.value !== '') {
@@ -24,6 +24,32 @@ function resetGame (e) {
   minField.value = ''
   maxField.value = ''
   guessInput.value = ''
+  minRangeNum.innerText = 1
+  maxRangeNum.innerText = 100
+  randomNum = Math.floor(Math.random() * 100) + 1
 }
 
+function updateValues (e) {
+  e.preventDefault()
+  var minVal = parseInt(minField.value, 10)
+  var maxVal = parseInt(maxField.value, 10)
+  randomNum = Math.floor(Math.random() * maxVal) + minVal
+  populateRangeText()
+  handleErrorMsg()
+}
 
+function populateRangeText () {
+  let minRangeNum = document.querySelector('.min-range-txt').innerText = minField.value
+  let maxRangeNum = document.querySelector('.max-range-txt').innerText = maxField.value
+}
+
+function handleErrorMsg () {
+  if(minField.value === '') {
+    document.querySelector('.min-error-msg').innerHTML = `<p class='error'>Please enter a min range</p>`
+    minField.style.border = '2px solid #e91b0d'
+  }
+  if(maxField.value === '') {
+    document.querySelector('.max-error-msg').innerHTML = `<p class='error'>Please enter a max range</p>`
+    maxField.style.border = '2px solid #e91b0d'
+  }
+}
