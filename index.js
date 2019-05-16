a5c96af881dcaa28dd64b861ba89841e1c7e3265
Sally Haefling <sallyhaefling@gmail.com>
2019-05-16 17:51:51 -0600

    FInish out js and styles

--- index.js
+++ index.js
@@ -7,15 +7,20 @@ let submitBtn = document.querySelector('.submit-btn')
 let clearBtn = document.querySelector('.clear-btn')
 let resetBtn = document.querySelector('.reset-btn')
 let rightDisplayNum = document.querySelector('.right-side-num')
+let message = document.querySelector('.error-message')
 
 resetBtn.addEventListener('click', resetGame)
 guessInput.addEventListener('keyup', enableButtons)
 updateBtn.addEventListener('click', updateValues)
+updateBtn.addEventListener('click', checkUserRange)
+submitBtn.addEventListener('click', getUserGuess)
 
 function enableButtons () {
   if(guessInput.value !== '') {
     resetBtn.removeAttribute('disabled')
+    resetBtn.style.color = '#e91b0d'
     clearBtn.removeAttribute('disabled')
+    clearBtn.style.color = '#e91b0d'
   }
 }
 
@@ -34,6 +39,7 @@ function updateValues (e) {
   var minVal = parseInt(minField.value, 10)
   var maxVal = parseInt(maxField.value, 10)
   randomNum = Math.floor(Math.random() * maxVal) + minVal
+  console.log(randomNum)
   populateRangeText()
   handleErrorMsg()
 }
@@ -51,5 +57,33 @@ function handleErrorMsg () {
   if(maxField.value === '') {
     document.querySelector('.max-error-msg').innerHTML = `Please enter a max range`
     maxField.style.border = '2px solid #e91b0d'
+  }
+}
+
+function checkUserRange () {
+  let minNumField = parseInt(minField.value)
+  let maxNumField = parseInt(maxField.value)
+  if(minNumField > maxNumField || maxNumField < minNumField) {
+    return message.innerText = 'Please try again'
+  }
+}
+
+function getUserGuess (e) {
+  e.preventDefault()
+  let currentGuess = parseInt(guessInput.value, 10)
+  let numDisplay = document.querySelector('.right-side-num')
+  let minValue = parseInt(minField.value)
+  let maxValue = parseInt(maxField.value)
+  numDisplay.innerText = currentGuess
+  if(currentGuess < minValue || currentGuess > maxValue) {
+    return message.innerText = 'You\'re outside of the range, please try again'
+  } else if (currentGuess < randomNum) {
+    return message.innerText = 'That\'s too low, try again'
+  } else if (currentGuess > randomNum) {
+    return message.innerText = 'That\'s too high, try again'
+  } else if (currentGuess === randomNum) {
+    return message.innerText = 'Yahoo! You won!'
+  } else {
+    return message.innerText = 'Enter a guess'
   }
 }
