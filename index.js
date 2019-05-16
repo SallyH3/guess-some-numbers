4f4536ef2fbe288b63580becd43bc0cb070ffff9
Sally Haefling <sallyhaefling@gmail.com>
2019-05-16 15:46:55 -0600

    Reset game working

--- index.js
+++ index.js
@@ -8,3 +8,22 @@ let submitBtn = document.querySelector('.submit-btn')
 let clearBtn = document.querySelector('.clear-btn')
 let resetBtn = document.querySelector('.reset-btn')
 let rightDisplayNum = document.querySelector('.right-side-num')
+
+resetBtn.addEventListener('click', resetGame)
+guessInput.addEventListener('keyup', enableButtons)
+
+function enableButtons () {
+  if(guessInput.value !== '') {
+    resetBtn.removeAttribute('disabled')
+    clearBtn.removeAttribute('disabled')
+  }
+}
+
+function resetGame (e) {
+  e.preventDefault()
+  minField.value = ''
+  maxField.value = ''
+  guessInput.value = ''
+}
+
+
